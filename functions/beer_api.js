6fe010851d628f41b19fcdfe8375d40ad2ef04ba
Stephen Zhao <xinghaozhaoex@gmail.com>
2018-05-25 22:49:00 -0400

    Auto stash before merge of "master" and "origin/master"

---
+++ functions/beer_api.js
@@ -0,0 +1,10 @@
+const lib = require('lib');
+/**
+* Implements Locmap Service to find coordinates
+* http://beermapping.com/webservice/locmap/60dd7689c776e989046ad20086dabe32/ID
+*
+* Also implements Loccity Service to find bars in city
+* http://beermapping.com/webservice/loccity/60dd7689c776e989046ad20086dabe32/lyons,co
+* @param {string} name Who you're saying hello to
+* @returns {string}
+*/
