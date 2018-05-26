cfcccccecf1b755da053c0d4db59f804b7d29a8e
Stephen Zhao <xinghaozhaoex@gmail.com>
2018-05-26 07:28:41 -0400

    Auto stash before merge of "master" and "origin/master"

--- functions/__main__.js
+++ functions/__main__.js
@@ -1,14 +1,46 @@
 //https://maps.googleapis.com/maps/api/geocode/json?address=University+of+Waterloo&key=AIzaSyC5sflggRGVfUwqg_yQKyqHkeCdwQFNGrI
 const request = require('request');
 const lib = require('lib');
-var url = 'https://maps.googleapis.com/maps/api/geocode/json?address=University+of+Waterloo&key=AIzaSyC5sflggRGVfUwqg_yQKyqHkeCdwQFNGrI';
 /**
 * A basic Hello World function
-* @param {string} address Street address of place
-* @returns {any}   long and lat of address
+* @param {integer} bars number of bars
+* @param {string} address address of person
+* @param {float} lat lattitude
+* @param {string} lng longitude
+* @returns {any}  JSON object of route
 */
-module.exports = (address, context, callback) => {
-	return lib[`${context.service.identifier}.gmap_api`](address , (err, result) => {
-    callback(err, result);
-  });
+module.exports = async (bars = 0, address = null,lat = null,lng = null, context) => {
+	//converting address to a long & lat
+	let result;
+	var data = [];
+	var tmplat;
+	var tmplng;
+	var x;
+	if (address != null) {
+		result = await lib[`${context.service.identifier}.gmap_api`]({
+			address: address
+		});
+		return result
+	}
+	else{
+		for(i=0;i<11 || i<bars;i++){
+			if(i==0){
+				tmplat = lat;
+				tmplng = lng;
+			}else if(i<11){
+				tmplat = data[i-1]["lat"];
+				tmplng = data[i-1]["lng"];
+			}
+				result = await lib[`${context.service.identifier}.get_route`]({
+					lat: tmplat,
+					lng: tmplng,
+				});
+
+				//for(x=0;x<11;x++){
+					//if(result["results"][x]["geometry"]["id"] in data){}
+				//}
+				data[i]= {"lat":result["results"][1]["geometry"]["location"]["lat"],"lng":result["results"][1]["geometry"]["location"]["lng"].toString()};
+		}
+		return data
+	}
 }
---
+++ functions/get_route.js
@@ -0,0 +1,17 @@
+const request = require('request');
+/**
+* A basic Hello World function
+* @param {float} lat
+* @param {string} lng
+* @returns {any} data long and lat of address
+*/
+module.exports = (lat= 43.5890, lng = "-79.6441", context,callback) => {
+  var url = `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${lat},${lng}&type=bar&rankby=distance&key=AIzaSyDcpknVA0AiNiR_zjO8EJpJmHm6gf9TtHE`;
+  var data = {};
+  request(url, function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      data = JSON.parse(body);
+      callback(null,data);
+    }
+  });
+}
