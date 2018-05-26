1837a79da8e921d0a43f1212247c47f865f8f84c
Stephen Zhao <xinghaozhaoex@gmail.com>
2018-05-26 00:51:33 -0400

    updated callback

--- functions/__main__.js
+++ functions/__main__.js
@@ -1,15 +1,14 @@
 //https://maps.googleapis.com/maps/api/geocode/json?address=University+of+Waterloo&key=AIzaSyC5sflggRGVfUwqg_yQKyqHkeCdwQFNGrI
 const request = require('request');
+const lib = require('lib');
 var url = 'https://maps.googleapis.com/maps/api/geocode/json?address=University+of+Waterloo&key=AIzaSyC5sflggRGVfUwqg_yQKyqHkeCdwQFNGrI';
 /**
 * A basic Hello World function
 * @param {string} loc Street address of place
-* @returns {any} body long and lat of address
+* @returns {any}   long and lat of address
 */
 module.exports = (loc = '', context, callback) => {
-	request(url, function (error, response, body) {
-    if (!error && response.statusCode == 200) {
-        callback(null,body);  // Print the google web page.
-     }
-	 })
+	return lib[`${context.service.identifier}.gmap_api`](loc , (err, result) => {
+    callback(err, result);
+  });
 }
--- functions/gmap_api.js
+++ functions/gmap_api.js
@@ -1,19 +1,16 @@
 //https://maps.googleapis.com/maps/api/geocode/json?address=University+of+Waterloo&key=AIzaSyC5sflggRGVfUwqg_yQKyqHkeCdwQFNGrI
-
-const https = require('https');
-const lib = require('lib');
-
+var url = 'https://maps.googleapis.com/maps/api/geocode/json?address=University+of+Waterloo&key=AIzaSyC5sflggRGVfUwqg_yQKyqHkeCdwQFNGrI';
+var data = {};
+const request = require('request');
 /**
 * A basic Hello World function
 * @param {string} loc Street address of place
 * @returns {object} data long and lat of address
 */
 module.exports = (loc = '', context, callback) => {
-	https.get('https://maps.googleapis.com/maps/api/geocode/json?address=University+of+Waterloo&key=AIzaSyC5sflggRGVfUwqg_yQKyqHkeCdwQFNGrI', (resp) => {
-		let data = '';
-		resp.on('data', (chunk) =>{
-			data +=chunk;
-		});
-	});
-	return data
+	request(url, function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+        callback(null,JSON.parse(body));
+     }
+	 });
 }
